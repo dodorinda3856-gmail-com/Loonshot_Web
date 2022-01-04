a145a0c8ca7bf6ba8b689fe2b18b95fbf467c81c
dodorinda <95510899+dodorinda@users.noreply.github.com>
2022-01-04 15:20:03 +0900

    모바일 프린터 테스트

--- LoonshotTest/Program.cs
+++ LoonshotTest/Program.cs
@@ -16,7 +16,6 @@ namespace LoonshotTest
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- LoonshotTest/Startup.cs
+++ LoonshotTest/Startup.cs
@@ -27,15 +27,15 @@ namespace LoonshotTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR(); 
-            
+            services.AddSignalR();
+
             services.Configure<WebEncoderOptions>(options =>
             {
                 options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All); // 한글이 인코딩되는 문제 해결
             });
 
             services.AddControllersWithViews();
-            services.AddAuthentication( options =>
+            services.AddAuthentication(options =>
             {
                 options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
