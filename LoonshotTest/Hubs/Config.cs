adee8381a8b49a4ec5d1cf127da0cc9f7dbf9d7f
dodorinda <95510899+dodorinda@users.noreply.github.com>
2022-01-11 11:23:42 +0900

    마이페이지 - 프린트 레이아웃 수정 메시지 기능 - api 아이디 변경

--- LoonshotTest/Hubs/Config.cs
+++ LoonshotTest/Hubs/Config.cs
@@ -12,8 +12,8 @@ static class Config
     //public static string apiKey = "NCSVIHHUD2JBAN0G";
     //public static string apiSecret = "E0KCIH7VODODTBVEX7SEJGEI0E6NQMY1";
     // ¿µÇå 
-    public static string apiKey = "NCSENK9RJHKZBKGK";
-    public static string apiSecret = "P0ZHGQDFSJABRRVTFJQTFEGKOKQVGPEK";
+    public static string apiKey = "NCSV8FXAG2HITRNS";
+    public static string apiSecret = "CNYDVWYSQOTTMEV4V2NQWQ71QKUQS0NC";
     public static string domain = "api.coolsms.co.kr";
     public static string protocol = "https";
     public static string prefix = "";
